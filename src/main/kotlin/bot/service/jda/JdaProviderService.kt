404b78243cc290b3fcaceabca280d0c385f85ee2
Zsolt Sajti <sajtizsolt@gmail.com>
2023-04-14 20:29:12 +0200

    fix: disable cache flag to remove warning

--- src/main/kotlin/bot/service/jda/JdaProviderService.kt
+++ src/main/kotlin/bot/service/jda/JdaProviderService.kt
@@ -1,12 +1,13 @@
 package bot.service.jda
 
+import bot.eventListener.MainEventListener
 import bot.service.configuration.ConfigurationService
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
-import net.dv8tion.jda.api.hooks.EventListener
 import net.dv8tion.jda.api.requests.GatewayIntent
 import net.dv8tion.jda.api.utils.MemberCachePolicy
+import net.dv8tion.jda.api.utils.cache.CacheFlag
 
 object JdaProviderService {
 
@@ -19,12 +20,13 @@ object JdaProviderService {
         return jda
     }
 
-    private fun buildJda(vararg listeners: EventListener): JDA {
+    private fun buildJda(): JDA {
         val jda = JDABuilder
             .create(ConfigurationService.getDiscordBotToken(), buildIntents())
             .setMemberCachePolicy(MemberCachePolicy.ALL)
             .enableIntents(GatewayIntent.MESSAGE_CONTENT)
-            .addEventListeners(listeners)
+            .disableCache(CacheFlag.SCHEDULED_EVENTS)
+            .addEventListeners(MainEventListener())
             .build()
             .awaitReady()
         jda.presence.activity = Activity.listening("your commands")
