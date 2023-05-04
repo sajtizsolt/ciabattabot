c9517e38bd03a5a3b9057f4a1aae813e82b9b54a
Zsolt Sajti <sajtizsolt@gmail.com>
2023-05-04 21:21:06 +0200

    refactor: move CommandManagerService to adequate package

--- src/main/kotlin/bot/service/command/CommandManagerService.kt
+++ src/main/kotlin/bot/service/command/CommandManagerService.kt
@@ -1,11 +1,14 @@
-package bot.command
+package bot.service.command
 
+import bot.command.Command
 import bot.commandmanagement.GeneralCommandManager
 import bot.domain.TextMessage
 
 object CommandManagerService {
 
     private val generalCommandManager = GeneralCommandManager()
+
+    private val commands = setOf<Command>()
 
     fun handleCommand(textMessage: TextMessage) {
         generalCommandManager.handleCommand(textMessage.event)
