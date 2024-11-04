cf670fe77b72bca404bfbad93a32d3972ff45f96
Zsolt Zoltán Sajti <sajtizsolt@gmail.com>
2024-11-04 16:31:45 +0100

    feat: make bot use po tokens

--- src/main/kotlin/bot/provider/audio/lavaPlayer/AudioPlayerManagerProvider.kt
+++ src/main/kotlin/bot/provider/audio/lavaPlayer/AudioPlayerManagerProvider.kt
@@ -1,9 +1,12 @@
 package bot.provider.audio.lavaPlayer
 
+import bot.service.configuration.ConfigurationService
 import com.sedmelluq.discord.lavaplayer.player.AudioConfiguration
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers
+import dev.lavalink.youtube.YoutubeAudioSourceManager
+import dev.lavalink.youtube.clients.Web
 
 object AudioPlayerManagerProvider {
 
@@ -24,6 +27,16 @@ object AudioPlayerManagerProvider {
         // TODO: Move this to configuration
         audioPlayerManager.configuration.resamplingQuality = AudioConfiguration.ResamplingQuality.HIGH
 
-        AudioSourceManagers.registerRemoteSources(audioPlayerManager)
+        // Register up-to-date source manager for YouTube
+        Web.setPoTokenAndVisitorData(
+            ConfigurationService.getPoToken(),
+            ConfigurationService.getPoVisitorData(),
+        )
+        val youtubeAudioSourceManager = YoutubeAudioSourceManager()
+        audioPlayerManager.registerSourceManager(youtubeAudioSourceManager)
+        AudioSourceManagers.registerRemoteSources(
+            audioPlayerManager,
+            com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager::class.java,
+        )
     }
 }
